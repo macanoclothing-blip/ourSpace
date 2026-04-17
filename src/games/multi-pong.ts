0902458438252c2de93f45e19fde92f6eb2abd03
skid <skid@kinkpad.local>
2026-04-17 13:19:52 +0200

    fixed pong aspect ratio

--- src/games/multi-pong.ts
+++ src/games/multi-pong.ts
@@ -4,9 +4,16 @@ import { GameClient, GameServer } from './game';
 
 const PLAYER_W = 0.04;
 const PLAYER_H = 0.15;
-
 const BALL_RADIUS = 0.03;
 const WINNING_SCORE = 100;
+const BORDERS = {
+    top: -1,
+    bottom: 1,
+    left: -2,
+    right: 2
+}
+const BORDERS_W = Math.abs(BORDERS.right - BORDERS.left);
+const BORDERS_H = Math.abs(BORDERS.top - BORDERS.bottom);
 
 export class PongServer extends GameServer {
 
@@ -22,9 +29,9 @@ export class PongServer extends GameServer {
         this.rightScore = 0;
 
         let i = 0;
-        let leftX = -0.97;
-        let rightX = 0.97 - PLAYER_W;
         const step = 0.08;
+        let leftX = BORDERS.left + step;
+        let rightX = BORDERS.right - step - PLAYER_W;
         Object.keys(players).forEach(id => {
             const player = players[id];
             if (i % 2 === 0) {
@@ -46,8 +53,8 @@ export class PongServer extends GameServer {
             const ball = {
                 x: 0,
                 y: 0,
-                vx: randomSignX * (0.1 + Math.random() * 0.35),
-                vy: randomSignY * (0.1 + Math.random() * 0.25)
+                vx: randomSignX * (0.1 + Math.random() * 0.35) * 2,
+                vy: randomSignY * (0.1 + Math.random() * 0.35)
             };
             this.balls.push(ball);
         }, 500);
@@ -73,19 +80,19 @@ export class PongServer extends GameServer {
             ball.y += ball.vy * dt;
 
             // collisioni palla/bordo
-            if (ball.y + BALL_RADIUS > 1) {
+            if (ball.y + BALL_RADIUS > BORDERS.bottom) {
                 ball.vy *= -1;
-                ball.y = 1 - BALL_RADIUS;
+                ball.y = BORDERS.bottom - BALL_RADIUS;
             }
-            else if (ball.y - BALL_RADIUS < -1) {
+            else if (ball.y - BALL_RADIUS < BORDERS.top) {
                 ball.vy *= -1;
-                ball.y = -1 + BALL_RADIUS;
+                ball.y = BORDERS.top + BALL_RADIUS;
             }
-            else if (ball.x + BALL_RADIUS > 1) {
+            else if (ball.x + BALL_RADIUS > BORDERS.right) {
                 this.leftScore += 1;
                 this.balls.splice(i, 1);
             }
-            else if (ball.x - BALL_RADIUS < -1) {
+            else if (ball.x - BALL_RADIUS < BORDERS.left) {
                 this.rightScore += 1;
                 this.balls.splice(i, 1);
             }
@@ -159,16 +166,23 @@ export class PongClient extends GameClient {
         const me = this.players[this.myId];
         me.y += moveDirectionY * dt * 1.3;
 
-        if (me.y < -1) me.y = -1;
-        else if (me.y + PLAYER_H > 1) me.y = 1 - PLAYER_H;
+        if (me.y < BORDERS.top) me.y = BORDERS.top;
+        else if (me.y + PLAYER_H > BORDERS.bottom) me.y = BORDERS.bottom - PLAYER_H;
         // -movimento
 
+
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, screenW, screenH);
+
         ctx.save();
-        ctx.translate(screenW/2, screenH/2); // (0,0) al centro
-        ctx.scale(screenW/2, screenH/2); // coordinate normalizzate
+        const scaleX = screenW / BORDERS_W;
+        const scaleY = screenH / BORDERS_H;
+        const scale = Math.min(scaleX, scaleY);
+        ctx.translate(screenW / 2, screenH / 2); 
+        ctx.scale(scale, scale);
 
         ctx.fillStyle = "#00820d";
-        ctx.fillRect(-1, -1, 2, 2);
+        ctx.fillRect(BORDERS.left, BORDERS.top, BORDERS_W, BORDERS_H);
 
         ctx.fillStyle = "#e1e1e1";
         ctx.fillRect(0, -1, 0.05, 2);
